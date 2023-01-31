73c6e53c58306fe60b5014212cd1c0d87b53b7c8
Monique Santos <moniquemattosm@gmail.com>
2023-01-31 12:04:31 +0100

    CHORE: rename file

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { AuthContextProvider } from "./store/AuthContext";
 import { MoviesContextProvider } from "./store/MoviesContext";
 import HomePage from "./views/Home";
 import Login from "./views/Login";
-import SignUp from "./views/Signup";
+import Register from "./views/Register";
 import UserProfile from "./Components/UserProfile";
 import ProtectedRoute from "./Components/routes/ProtectedRoute";
 import { app } from "./config/firebaseConfig";
@@ -15,30 +15,28 @@ import { app } from "./config/firebaseConfig";
 // in the Auth context , create login function, export it to login component. You should see a console log in the auth context displaying user/email and password
 
 function App() {
-
   console.log(app);
   return (
     <div>
       <AuthContextProvider>
-      <MoviesContextProvider>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="signup" element={<SignUp />} />
-          <Route path="login" element={<Login />} />
-          <Route path="userprofile" element={<UserProfile />} />
-          <Route 
-            path="userprofile" 
-            element={
-            <ProtectedRoute>
-            <UserProfile />
-            </ProtectedRoute>
-           }
-           />
-        </Routes>
-      </MoviesContextProvider>
-    
+        <MoviesContextProvider>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="register" element={<Register />} />
+            <Route path="login" element={<Login />} />
+            <Route path="userprofile" element={<UserProfile />} />
+            <Route
+              path="userprofile"
+              element={
+                <ProtectedRoute>
+                  <UserProfile />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+        </MoviesContextProvider>
       </AuthContextProvider>
-      </div>
+    </div>
   );
 }
 
