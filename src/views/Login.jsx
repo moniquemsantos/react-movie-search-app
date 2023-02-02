6051406591174482f6a430ab1fe9f224f20477a5
Monique Santos <moniquemattosm@gmail.com>
2023-02-02 01:23:00 +0100

    FIX: App bar styles, and login behavior

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,74 +1,98 @@
-import React, {useContext, useState} from 'react'
-import { Link } from 'react-router-dom';
-import { Avatar, Button, Grid, Paper, TextField, Typography } from '@mui/material'
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import { red } from '@mui/material/colors';
-import { AuthContext } from '../store/AuthContext';
-import Header from '../Components/Header';
-
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  Avatar,
+  Button,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { red } from "@mui/material/colors";
+import { AuthContext } from "../store/AuthContext";
+import Header from "../Components/Header";
 
 const Login = () => {
-  const paperStyle={padding: 20, width: 300, height: "52vh", margin:"40px auto"};
-  const avatarStyle={backgroundColor:"#ff0000"};
-  const btnStyle={margin: "8px 0", backgroundColor: red[500]};
-  const linkStyle={margin: "8px"};
+  const navigate = useNavigate();
+  const paperStyle = {
+    padding: 20,
+    width: 300,
+    height: "52vh",
+    margin: "40px auto",
+  };
+  const avatarStyle = { backgroundColor: "#ff0000" };
+  const btnStyle = { margin: "8px 0", backgroundColor: red[500] };
+  const linkStyle = { margin: "8px" };
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const { login }=useContext(AuthContext);
+  const { login } = useContext(AuthContext);
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value)
-    };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value)
-    };
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handleRegister = (event) => {
-        // DO YOUR DUE DILIGENCE HERE: check email format, and maybe password length
-        event.preventDefault()
-        login(email, password)
-    };
-  return(
+  const handleLogin = async (event) => {
+    // DO YOUR DUE DILIGENCE HERE: check email format, and maybe password length
+    event.preventDefault();
+    await login(email, password).then((loggedUser) => {
+      if (loggedUser) {
+        navigate("/");
+      }
+    });
+  };
+  return (
     <>
-     <Grid>
-        < Header />
-      <Paper style={paperStyle}>
+      <Grid>
+        <Header />
+        <Paper style={paperStyle}>
           <Grid align="center">
-          <Avatar style={avatarStyle}>
-              <LockOutlinedIcon/>
-           </Avatar>
-              <h2>Login</h2>
+            <Avatar style={avatarStyle}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <h2>Login</h2>
           </Grid>
-          <TextField 
-          variant="standard" 
-          label="Email" 
-          id = "email"
-          fullWidth 
-          required
-          onChange={handleEmailChange}
+          <TextField
+            variant="standard"
+            label="Email"
+            id="email"
+            fullWidth
+            required
+            onChange={handleEmailChange}
           />
-          <TextField 
-          variant="standard" 
-          label="Password"
-          type="password"
-          fullWidth 
-          required
-          onChange={handlePasswordChange}
+          <TextField
+            variant="standard"
+            label="Password"
+            type="password"
+            fullWidth
+            required
+            onChange={handlePasswordChange}
           />
-          <Button onClick={handleRegister} type="submit" variant="contained" style={btnStyle} fullWidth>Login</Button>
-          <Typography> Do you have an account?
-              <Link to="/register" style={linkStyle} 
-             >
-                 Sign Up
-                  </Link>
+          <Button
+            onClick={handleLogin}
+            type="submit"
+            variant="contained"
+            style={btnStyle}
+            fullWidth
+          >
+            Login
+          </Button>
+          <Typography>
+            {" "}
+            Do you have an account?
+            <Link to="/register" style={linkStyle}>
+              Sign Up
+            </Link>
           </Typography>
-      </Paper>
-
-     </Grid>
-     </>
-  )
-}
+        </Paper>
+      </Grid>
+    </>
+  );
+};
 
 export default Login;
